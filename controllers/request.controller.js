566dcc3b9ad3724fd692683656405d7607920ab3
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-11-19 15:11:01 +0700

    feat: request

--- controllers/request.controller.js
+++ controllers/request.controller.js
@@ -13,9 +13,10 @@ class RequestController {
 
   async getRequestsByUser(req, res) {
     const { userId } = req.params;
-    const listRequest = await RequestModel.find({ uId: userId })
-      .populate("User")
-      .populate("Book");
+    let listRequest = await RequestModel.find({})
+      .populate("user")
+      .populate("book");
+    listRequest = listRequest.filter((item) => item.book.uId === userId);
     res.status(200).send({
       code: 0,
       message: "Get list success",
--- routes/request.route.js
+++ routes/request.route.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const requestController = require("../controllers/request.controller");
 router.post("/create-request", requestController.createRequest);
+router.get("/get-request/:userId", requestController.getRequestsByUser);
 
 module.exports = router;

4bbe86772f633c23d5e33d1a6eff5ea2293d49b4
Minh Hiển Nguyễn <minhhien24022001@gmail.com>
2022-11-19 13:39:04 +0700

    feat: add api of get all available books and get all books by user

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,4 +1,5 @@
 const BookModel = require("../models/book.model");
+const UserModel = require("../models/user.model");
 
 class BookController {
   async uploadBook(req, res) {
@@ -12,6 +13,39 @@ class BookController {
       });
     } catch (error) {
       res.status(500).send({ error });
+    }
+  }
+  async getAllAvailableBooks(req, res) {
+    try {
+      const data = [];
+      const users = await UserModel.find({});
+      for (let user of users) {
+        const books = await BookModel.find({uId: user._id});
+        const availableBooks = books.filter((item) => item.isHaving);
+        data.push({
+          uId: user._id,
+          name: user.name,
+          availableBooks: availableBooks,
+        })
+      }
+      res.status(200).send(data)
+    } catch (error) {
+      res.status(500).send({error})
+    }
+  }
+  async getAllBooksByUser(req, res) {
+    try {
+      const uId = req.params.uId;
+      const books = await BookModel.find({uId: uId});
+      const availableBooks = books.filter((item) => item.isHaving);
+      const wishBooks = books.filter((item) => !item.isHaving);
+      const data = {
+        availableBooks: availableBooks,
+        wishBooks: wishBooks
+      }
+      res.status(200).send(data)
+    } catch (error) {
+      res.status(500).send({error})
     }
   }
 }
--- routes/book.route.js
+++ routes/book.route.js
@@ -4,5 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post("/upload-book", bookController.uploadBook);
+router.get("/get-all-available-books", bookController.getAllAvailableBooks);
+router.get("/get-all-books/:uId", bookController.getAllBooksByUser);
 
 module.exports = router;

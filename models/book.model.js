4bcef8bfa0aa2f0e60d6a38cd5813a7da10b8199
unknown <lechuan2001@gmail.com>
2022-11-19 10:39:32 +0700

    book_schema

---
+++ models/book.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const schema = new mongoose.Schema({
+    uId: {
+        type: String,
+        required: true,
+    },
+    name:{
+        type: String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required: false,
+    },
+    image:{
+        type:String,
+        required: true,  
+    },
+    isHaving:{
+        type:Boolean,
+        default:true,
+    },
+});
